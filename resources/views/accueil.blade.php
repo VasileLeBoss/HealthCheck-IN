f17abd7b785b4b333763c010791ed9200c391e2b
unknown <vasile.bors.03@gmail.com>
2024-04-16 01:27:57 +0200

    'v0.2'

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -50,11 +50,29 @@
         </div>
 
         <div class="relative flex flex-column justify-between min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-main">
+        <div>
         <div class="p-6 flex justify-end">
 
         <h2 class="text-xl font-semibold text-gray-900 dark:text-white">Bienvenue, {{ auth()->user()->prenom }}</h2>
 
         </div>   
+            <div class="grid md:grid-cols-2 gap-6 p-6">
+                <a href="" class="overflow-hidden">
+                <div class="relative p-6 text-white min-h-300px bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex  transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+                    <h1 class="text-h1  new-mark">Médicaments</h1>
+                </div>
+                </a>
+                <a href="">
+                <div class=" p-6 text-white min-h-300px bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex  transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+                    <h1 class="text-h1 ">Statistiques</h1>
+                </div>
+                </a>
+                <div class=" p-6 span-2 text-white min-h-300px bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex  transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+                    <h1 class="text-h1 ">Mes Rapport</h1>
+                </div>
+                
+            </div>
+        </div>
         <!-- <div class=" p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex  transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
 
                         </div> -->
